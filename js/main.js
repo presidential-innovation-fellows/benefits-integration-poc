0528635f203dba74baabf1c66bdbdac4ff389f47
Ross Dakin <rossdakin@gmail.com>
2016-04-21 12:50:36 -0400

    Add auto-population button

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-alert('This is an internal demo site currently in alpha. This is not real.');
+jQuery(function($) {
+  $('#populate').click(function() {
+    $('input#first-name').val('Mark');
+    $('input#middle-name').val(null);
+    $('input#last-name').val('White');
+    $('input#date_of_birth_1').val(7);
+    $('input#date_of_birth_2').val(8);
+    $('input#date_of_birth_3').val(1991);
+    $('input#ssn_1').val('277');
+    $('input#ssn_2').val('02');
+    $('input#ssn_3').val('5100');
+    $('input#citizen').prop('checked', true);
+    $('input#title2').prop('checked', true);
+  });
+});
